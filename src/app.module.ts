3a5cbde9c084006877aa8fe4596eaf38ad09ac25
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-14 23:36:16 -0300

    feat: created Api messages module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { NotificationApiModule } from './notification-api/notification-api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NotificationApiModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
