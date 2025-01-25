502fc8e7b34068d3939ac9e4e1a927c9df4599dc
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-24 23:43:46 -0300

    feaT: Added TypeORM in the Message module

--- src/notification-api/notification-api.module.ts
+++ src/notification-api/notification-api.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationApiController } from './notification-api.controller';
 import { NotificationApiService } from './notification-api.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from './Entities/message.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Message])],
   controllers: [NotificationApiController],
   providers: [NotificationApiService],
 })
