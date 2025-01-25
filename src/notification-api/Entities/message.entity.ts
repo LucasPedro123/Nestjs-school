d408302cd245e570b537b864cf8790461ab37b14
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-24 23:42:40 -0300

    feat: Added database entity

--- src/notification-api/Entities/message.entity.ts
+++ src/notification-api/Entities/message.entity.ts
@@ -1,9 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Message {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   text: string;
+
+  @Column({ type: 'varchar', length: 50 })
   by: string;
+
+  @Column()
   date: Date;
-  user: string;
+
+  @Column()
+  createdAt?: Date;
+
+  @Column()
+  updatedAt?: Date;
+
+  @Column({ type: 'varchar', length: 50 })
   to: string;
+
+  @Column({ type: 'boolean' })
   read: boolean;
 }
