bd947b406581961ee309f2e8995bbac98a590749
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-24 23:41:17 -0300

    feat: added postgreSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { NotificationApiModule } from './notification-api/notification-api.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [NotificationApiModule],
+  imports: [
+    NotificationApiModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'luc4s123',
+      database: 'postgres',
+      autoLoadEntities: true, //carega entidade sem precisar especificar
+      synchronize: true, //sincroniza as tabelas com o banco
+    }),
+  ],
   controllers: [],
   providers: [],
 })
