5686624d46c17e4e055e8d9698bcb53d7ee8230b
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-26 22:31:02 -0300

    feat(notification): added property extension to updated DTO

--- src/notification-api/dto/update-message.dto.ts
+++ src/notification-api/dto/update-message.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { createMessageDto } from './create-message.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateMessageDto extends PartialType(createMessageDto) {}
+export class UpdateMessageDto extends PartialType(createMessageDto) {
+  @IsBoolean()
+  @IsOptional()
+  read?: boolean;
+}
