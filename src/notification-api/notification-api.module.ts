a074c5663bb9885b767264e3b93125b9ed70fe04
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-01-31 22:46:48 -0300

    feat(notification-api): added Guard and Filter

--- src/notification-api/notification-api.module.ts
+++ src/notification-api/notification-api.module.ts
@@ -4,10 +4,23 @@ import { NotificationApiService } from './notification-api.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './Entities/message.entity';
 import { UsersModule } from 'src/users/users.module';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { MyExceptionFilter } from 'src/common/filters/MyException.filter';
+import { IsAdminGuard } from 'src/common/Guards/is-admin.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message]), UsersModule],
   controllers: [NotificationApiController],
-  providers: [NotificationApiService],
+  providers: [
+    NotificationApiService,
+    {
+      provide: APP_FILTER,
+      useClass: MyExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: IsAdminGuard,
+    },
+  ],
 })
 export class NotificationApiModule {}
