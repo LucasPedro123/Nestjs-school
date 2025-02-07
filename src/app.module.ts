902aebd22741671c4f2300e1af26be907c3b6799
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-06 23:55:05 -0300

    chore: added environment variable setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,21 @@ import { NotificationApiModule } from './notification-api/notification-api.modul
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { SimpleMiddleware } from './common/middleware/simple.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['/env/.env'],
+    }),
     NotificationApiModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'luc4s123',
-      database: 'postgres',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       autoLoadEntities: true, //carega entidade sem precisar especificar
       synchronize: true, //sincroniza as tabelas com o banco
     }),
