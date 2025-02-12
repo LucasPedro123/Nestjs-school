94631987155839d9027b1100bd0980de6d7cdb7f
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-11 23:58:14 -0300

    feat: added AuthTokenGuard to Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { AuthTokenGuard } from 'src/auth/guard/auth.guard';
+import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
+import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -20,6 +23,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
@@ -30,13 +34,23 @@ export class UsersController {
     return this.usersService.findOne(+id);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @TokenPayloadParam() tokenPayloadParam: TokenPayloadDto,
+  ) {
+    console.log('AQUI: ', tokenPayloadParam);
+    return this.usersService.update(+id, updateUserDto, tokenPayloadParam);
   }
 
+  @UseGuards(AuthTokenGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  remove(
+    @Param('id') id: string,
+    @TokenPayloadParam() tokenPayloadParam: TokenPayloadDto,
+  ) {
+    return this.usersService.remove(+id, tokenPayloadParam);
   }
 }
