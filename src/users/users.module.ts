1ffbd6fd8d5fc9be735d9b4527378ed66d4caae1
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-12 00:01:31 -0300

    feat: added Auth module dependencies to Users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
