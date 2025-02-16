1d63059784f6c68bf3ab640aa3f6c2b02c4b00a9
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-15 23:53:04 -0300

    feat(Service): added user search by token in Notification

--- src/notification-api/notification-api.service.ts
+++ src/notification-api/notification-api.service.ts
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UsersService } from 'src/users/users.service';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
 
 @Injectable()
 export class NotificationApiService {
@@ -51,21 +52,51 @@ export class NotificationApiService {
       : new HttpException('Item não encontrado!', HttpStatus.NOT_FOUND);
   }
 
-  async updateMessage(id: number, body: UpdateMessageDto) {
+  async updateMessage(
+    id: number,
+    body: UpdateMessageDto,
+    tokenPayload: TokenPayloadDto,
+  ) {
+    const tokenIdUser = tokenPayload.sub;
     const messageFindById = await this.messageRepository.findOne({
       where: {
         id,
       },
+      relations: ['byId', 'toId'],
+      order: {
+        id: 'DESC',
+      },
+      select: {
+        byId: {
+          id: true,
+          name: true,
+        },
+        toId: {
+          id: true,
+          name: true,
+        },
+      },
     });
 
-    messageFindById.text = body.text;
-    messageFindById.read = body.read;
-    this.messageRepository.save(messageFindById);
-    return messageFindById;
+    if (messageFindById.byId.id == tokenIdUser) {
+      messageFindById.text = body.text;
+      messageFindById.read = body.read;
+      this.messageRepository.save(messageFindById);
+      return messageFindById;
+    } else {
+      return new HttpException(
+        'Você não tem permissão para editar essa mensagem!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
   }
 
-  async createMessage(body: createMessageDto) {
-    const { byId, toId } = body;
+  async createMessage(
+    body: createMessageDto,
+    tokenPayloadDto: TokenPayloadDto,
+  ) {
+    const { toId } = body;
+    const byId = tokenPayloadDto.sub;
 
     const searchUserSenderById = await this.userService.findOne(byId);
     const searchUserReceiverById = await this.userService.findOne(toId);
@@ -87,21 +118,51 @@ export class NotificationApiService {
       ...messageNew,
       byId: {
         id: searchUserSenderById.id,
+        name: searchUserReceiverById.name,
       },
       toId: {
         id: searchUserReceiverById.id,
+        name: searchUserReceiverById.name,
       },
     };
   }
 
-  async deleteMessage(id: number) {
-    const findIndexMessage = await this.messageRepository.findOneBy({
-      id,
+  async deleteMessage(id: number, tokenPayload: TokenPayloadDto) {
+    const messageFindById = await this.messageRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['byId', 'toId'],
+      order: {
+        id: 'DESC',
+      },
+      select: {
+        byId: {
+          id: true,
+          name: true,
+        },
+        toId: {
+          id: true,
+          name: true,
+        },
+      },
     });
 
-    if (!findIndexMessage) {
+    if (!messageFindById) {
       throw new HttpException('Item não encontrado!', HttpStatus.NOT_FOUND);
     }
-    this.messageRepository.remove(findIndexMessage);
+
+    if (messageFindById.byId.id == tokenPayload.sub) {
+      this.messageRepository.remove(messageFindById);
+      return {
+        message: `A messagem de ${messageFindById.byId.name} foi deletada com sucesso`,
+        status: 200,
+      };
+    } else {
+      throw new HttpException(
+        'Você não tem permissão para deletar essa mensagem!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
   }
 }
