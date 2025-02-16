0c045e36c614d23c2122fbd4ff9266d994b55549
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-15 23:51:24 -0300

    feat: added token validation in Notification

--- src/notification-api/notification-api.module.ts
+++ src/notification-api/notification-api.module.ts
@@ -4,22 +4,25 @@ import { NotificationApiService } from './notification-api.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './Entities/message.entity';
 import { UsersModule } from 'src/users/users.module';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER } from '@nestjs/core';
 import { MyExceptionFilter } from 'src/common/filters/MyException.filter';
-import { IsAdminGuard } from 'src/common/Guards/is-admin.guard';
+import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    UsersModule,
+    AuthModule,
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [NotificationApiController],
   providers: [
     NotificationApiService,
     {
       provide: APP_FILTER,
       useClass: MyExceptionFilter,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: IsAdminGuard,
     },
   ],
 })
